892101c8ef53493bb969e2fb699f41ef155c7cd0
Carolyn Smith <carolynsmith@slu.edu>
2025-03-17 14:26:18 -0500

    initial push

---
+++ src/main/java/org/launchcode/sumits_webpage/SumitsWebpageApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.sumits_webpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumitsWebpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SumitsWebpageApplication.class, args);
+	}
+
+}
